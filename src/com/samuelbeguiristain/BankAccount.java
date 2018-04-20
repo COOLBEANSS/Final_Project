1ab1fb3910be9428762bdd0107b101f7ee496436
Samuel Beguiristain <dev@begui.me>
2018-04-20 14:58:39 -0400

    Initial Commit.
    Signed-off-by: Samuel Beguiristain <dev@begui.me>

---
+++ src/com/samuelbeguiristain/BankAccount.java
@@ -0,0 +1,17 @@
+/**
+ * AUTHOR: SAMUEL A. BEGUIRISTAIN
+ * DATE: APRIL 20th, 2018
+ * STUDENT ID: 1001532454
+ * COP2800-2183-2912
+ * FINAL PROJECT
+ * */
+
+package com.samuelbeguiristain;
+
+public class BankAccount {
+
+    public class SavingAccount extends BankAccount{
+
+    }
+
+}
---
+++ src/com/samuelbeguiristain/Main.java
@@ -0,0 +1,16 @@
+/**
+ * AUTHOR: SAMUEL A. BEGUIRISTAIN
+ * DATE: APRIL 20th, 2018
+ * STUDENT ID: 1001532454
+ * COP2800-2183-2912
+ * FINAL PROJECT
+ * */
+
+package com.samuelbeguiristain;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
