01d101db4bd4755d62bd07c385006a3a0db860d0
Samuel Beguiristain <dev@begui.me>
2018-04-29 19:50:50 -0400

    Adding author information.
    Signed-off-by: Samuel Beguiristain <dev@begui.me>

--- src/com/samuelbeguiristain/Main.java
+++ src/com/samuelbeguiristain/Main.java
@@ -1,3 +1,10 @@
+/**
+ * AUTHOR: SAMUEL A. BEGUIRISTAIN
+ * DATE: APRIL 20th, 2018
+ * STUDENT ID: 1001532454
+ * COP2800-2183-2912
+ * FINAL PROJECT
+ * */
 package com.samuelbeguiristain;
 
 import javafx.application.Application;
